a6e8dbcde4a956904bbaaaf4107691c2aab917e7
qrvck <v.kapitan2010@gmail.com>
2021-06-14 14:17:47 +0300

    доработал функцию случайных чисел

--- js/main.js
+++ js/main.js
@@ -13,11 +13,13 @@ getLineLength();
 const randomNumber = function (min, max) {
 
   if (max <= min) {
-    return false;
+    const TEMP = min;
+    min = max;
+    max = TEMP;
   }
 
   const RAND = min + Math.random() * (max + 1 - min);
-  return Math.floor(RAND);
+  return Math.abs(Math.floor(RAND));
 
 };
 
